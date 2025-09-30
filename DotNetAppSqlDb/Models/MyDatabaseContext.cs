029a61d2daaff9ce438dd578c01ecb19ff54dabc
Ayoub Khloufi <2531003@etu.cchic.ca>
2025-09-30 16:19:17 -0400

    Update connectionString

--- DotNetAppSqlDb/Models/MyDatabaseContext.cs
+++ DotNetAppSqlDb/Models/MyDatabaseContext.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.SqlServer;
 using System.Linq;
 using System.Web;
 
 namespace DotNetAppSqlDb.Models
 {
+    [DbConfigurationType(typeof(AppServiceConfiguration))]
     public class MyDatabaseContext : DbContext
     {
         // You can add custom code to this file. Changes will not be overwritten.
@@ -20,5 +22,15 @@ namespace DotNetAppSqlDb.Models
         }
 
         public System.Data.Entity.DbSet<DotNetAppSqlDb.Models.Todo> Todoes { get; set; }
+
+        public class AppServiceConfiguration : MicrosoftSqlDbConfiguration
+        {
+            public AppServiceConfiguration()
+            {
+                SetProviderFactory("System.Data.SqlClient", Microsoft.Data.SqlClient.SqlClientFactory.Instance);
+                SetProviderServices("System.Data.SqlClient", MicrosoftSqlProviderServices.Instance);
+                SetExecutionStrategy("System.Data.SqlClient", () => new MicrosoftSqlAzureExecutionStrategy());
+            }
+        }
     }
 }
